a449d12b560109bf3a569bcc5d57d60794050587
Arturo Contreras Torres <165977656+Arturouu@users.noreply.github.com>
2026-06-09 23:36:41 -0500

    feat(environment): update development environment.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:3000/api/v1',
+  serverBasePath: 'bloomie-frontend-mockapi.azurewebsites.net',
   usersEndpointPath: '/users',
   sessionsEndpointPath: '/sessions',
   skinProfilesEndpointPath: '/skin_profiles',
