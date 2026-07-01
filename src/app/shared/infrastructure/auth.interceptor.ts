c18c28b2bdfff4ae4d82a57cb7ece5926dd44f53
luuu6 <lucianamechan@gmail.com>
2026-07-01 14:47:07 -0500

    fix(iam): update authentication interceptor

--- src/app/shared/infrastructure/auth.interceptor.ts
+++ src/app/shared/infrastructure/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('authToken');
+  const token = sessionStorage.getItem('authToken');
   if (token) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`),
